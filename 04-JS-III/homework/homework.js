1320e559d911e24e4a60ca7f9be5e8429bfea3bd
jessicaschwab <schwabjessica@gmail.com>
2021-07-11 19:57:11 -0300

    AGREGUE PROMEDIO

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return   array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array.pop();
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+ return  array.length;
 }
 
 
@@ -23,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+for(let i=0;i<array.length;i++){
+array[i]=array[i]+1;
+
+}
+return array;
 }
 
 
@@ -30,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+   array.push(elemento);
+   return array;
 }
 
 
@@ -38,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,20 +59,39 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+   var rta = palabras[0];
+  for(var i=1;i<palabras.length;i++){
+ rta =rta + ' ' + palabras[i];
+   }
+return rta;
+
+
+
 }
 
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
-  // Devuelve "true" si está, o "false" si no está
+  // Devuelv0e "true" si está, o "false" si no está
   // Tu código:
+for(let i=0; i<array.length ; i++){
+if (array[i] == elemento){
+  return true;
 }
-
+}
+return false;
+}
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma=0;
+  for(let i=0;i<numeros.length;i++ ){
+    suma=suma+numeros[i];
+    
+  }
+  return suma;
 }
 
 
@@ -68,6 +99,17 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+
+  var promedio=0;
+  var suma=0;
+var cantidad= resultadosTest.length;
+  for(let i=0;i<cantidad;i++){
+  
+   suma=suma+resultadosTest[i];
+  
+  }
+   promedio=suma/cantidad;
+  return promedio;
 }
 
 
